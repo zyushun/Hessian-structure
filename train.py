6ee63ab937034aebe48658752d8f21e876e46e5e
Yushun Zhang <85537560+zyushun@users.noreply.github.com>
2025-04-03 16:50:22 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -11,72 +11,67 @@ import seaborn as sns
 
 
 # Hyperparameters
-n_samples_per_class = 10
 n_total = 5000
-n_classes = 500
-input_dim = 500 
+n_classes =  500
+n_clusters = 500
+input_dim = 500
 width = 8
 num_epochs = 0
 cosine_epochs = 50000 
 lr = 1e-4
-model_type = 'nn' #'linear'
-visual_degree = 50 #  hyperparamter for visualization: heatmap_max = hessian_max / visual_degree
+model_type = 'nn' 
+visual_degree = 100# hyperparamter for visualization: heatmap_max = hessian_max / visual_degree
 visual_degree_block_hessian = [10, 100]
 loss_type = 'CE'
 optimizer_type = 'adam'
 cmap = 'cividis'
-gaussian_data = True#False
-comment = 'gaussian-n_total-'+str(n_total)+'-'+model_type+'-dim-'+str(input_dim)+'-width-'+str(width)+'-class-'+str(n_classes)+ '-sample_per_class-'+str(n_samples_per_class)+'-adam-'+loss_type+'visiondegree'+str(visual_degree)+'T'+str(num_epochs)+'optimizer-'+optimizer_type +'lr-'+str(lr)
+gaussian_data = False
+comment = 'clusterd-data-n_total-'+str(n_total)+'-'+model_type+'-dim-'+str(input_dim)+'-width-'+str(width)+'-cluster-'+ str(n_clusters)+'-class-'+str(n_classes)+'-adam-'+loss_type+'visiondegree'+str(visual_degree)+'T'+str(num_epochs)+'optimizer-'+optimizer_type +'lr-'+str(lr)
 
 torch.manual_seed(0)
 np.random.seed(0)
 
 
 
-
-def generate_data(n_samples_per_class, n_classes, input_dim):
-    # Generate synthetic data for specified dimensions
+def generate_cluster_data(n_total, n_classes, n_clusters, input_dim):
+    # Generate C-cluster synthetic data for specified dimensions
+    # used for ablation study with Li et al. 19 https://arxiv.org/pdf/1903.11680, as suggested by Reviewer 1.
+    # n_total is the total number of samples
+    # n_classes is the number of classes (smaller than n_clusters)
+    # input_dim is the dimension of the data
+    # raise error if n_cluster is larger than n_classes
+    assert n_classes<= n_clusters, f"n_cluster = {n_classes} is not smaller than n_classes = {n_classes}"
+    
+    # n_samples_per_class is the number of samples per class
     X = []
     y = []
-    for i in range(n_classes):
+    n_cluster_per_class = n_clusters // n_classes
+    n_samples_per_cluster = n_total // n_clusters
+    cluster_idx = 0
+    for class_idx in range(n_classes):
 
+        for _ in range(n_cluster_per_class):
+            cluster_idx += 1
+            if input_dim == 2:
+                center = np.array([np.cos(2 * np.pi * cluster_idx / n_clusters), np.sin(2 * np.pi * (cluster_idx) / n_clusters)]) * 5  # Class centers on a circle
+            else:
+                #extend the 2D case to higher dimension
+                # Generate random points in higher dimensions and project onto hypersphere
+                center = np.random.randn(input_dim)
+                # Normalize to create a unit vector (point on unit hypersphere)
+                center = center / np.linalg.norm(center)
 
-        if input_dim == 2:
-            center = np.array([np.cos(2 * np.pi * i / n_classes), np.sin(2 * np.pi * i / n_classes)]) * 5  # Class centers on a circle
-        else:
-            center = np.random.rand(input_dim) * 10  # Random class center
-        class_samples = np.random.randn(n_samples_per_class, input_dim) * 0.5 + center  # Add some noise
-        X.append(class_samples)
-
-        # assign random label
-        y.extend([np.random.randint(0, n_classes)]*n_samples_per_class)
-        # y.extend([i] * n_samples_per_class)
+            cluster_samples = np.random.randn(n_samples_per_cluster, input_dim) * 0.05 + center  # Add some noise
+            X.append(cluster_samples)
+            # assign label
+            y.extend([class_idx]*n_samples_per_cluster)
 
     X = np.vstack(X)  # Combine all class samples
     y = np.array(y)    # Convert labels to a NumPy array
     return X, y
 
 
-# generate Gaussian data
-def generate_gaussian_data(n_samples_per_class, n_classes, input_dim):
-    # Generate synthetic data for specified dimensions
-    X = []
-    y = []
-    for i in range(n_classes):
-        class_samples = np.random.randn(n_samples_per_class, input_dim) 
-        X.append(class_samples)
-        y.extend([i] * n_samples_per_class) # not used
-
-    X = np.vstack(X)  # Combine all class samples
-    y = np.array(y)    # Convert labels to a NumPy array
-    return X, y
-
-
-
-
-
-# generate Gaussian data, single sample
-def generate_gaussian_data_single_sample(n_total, n_classes, input_dim):
+def generate_gaussian_data(n_total, n_classes, input_dim):
     # Generate synthetic data for specified dimensions
     X = []
     y = []
@@ -93,10 +88,9 @@ def generate_gaussian_data_single_sample(n_total, n_classes, input_dim):
 
 # Generate synthetic data
 if gaussian_data:
-    #X, y = generate_gaussian_data(n_samples_per_class, n_classes, input_dim)
-    X, y = generate_gaussian_data_single_sample(n_total, n_classes, input_dim)
+    X, y = generate_gaussian_data(n_total, n_classes, input_dim)
 else:
-    X, y = generate_data(n_samples_per_class, n_classes, input_dim)
+    X, y = generate_cluster_data(n_total, n_classes, n_clusters, input_dim)
 # Convert to PyTorch tensors
 X_tensor = torch.from_numpy(X).float()
 y_tensor = torch.from_numpy(y)  # Use class indices directly
@@ -132,6 +126,7 @@ class SimpleNN(nn.Module):
     
 
 
+
 class linearNN(nn.Module):
     def __init__(self):
         super(linearNN, self).__init__()
@@ -165,8 +160,6 @@ if model_type == 'nn':
 elif model_type == 'linear':
     model = linearNN()
 
-
-
 if loss_type == 'CE':
     criterion = nn.CrossEntropyLoss()  # Cross Entropy Loss
 elif loss_type == 'MSE':
@@ -179,7 +172,7 @@ if optimizer_type == 'adam':
     optimizer = optim.Adam(model.parameters(), lr = lr)
 elif optimizer_type == 'sgd':
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum = 0)
-# cosine learnin rate
+
 # Training loop
 scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=cosine_epochs,eta_min = 0) #lr / 10)
 
@@ -195,14 +188,13 @@ for epoch in range(num_epochs):
     optimizer.step()  # Update weights
     scheduler.step()
 
-    avg_loss = loss.item() / (n_samples_per_class * n_classes)
+    avg_loss = loss.item() / (n_total)
     loss_list.append(avg_loss)
     if (epoch + 1) % 10 == 0:
         print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {avg_loss:.4f}')
 
 # Visualization
-
-def plot_results(): # might not use this
+def plot_dataset(): # might not use this
     with torch.no_grad():
         model.eval()
         outputs = model(X_tensor)
@@ -213,29 +205,17 @@ def plot_results(): # might not use this
         # accuracy
         accuracy = (predicted_cls == groundtruth_cls).mean()
         print(f'Accuracy: {accuracy:.2f}')
-
-
     plt.figure()
-    plt.scatter(X[:, 0], X[:, 1], c=predicted_cls, cmap='coolwarm', alpha=0.5)
+    # plt.scatter(X[:, 0], X[:, 1], c=predicted_cls, cmap='coolwarm', alpha=0.5)
 
     plt.scatter(X[:, 0], X[:, 1], c=groundtruth_cls, cmap='coolwarm', alpha=0.5,marker='x')
     plt.title('Classification Results')
     plt.xlabel('Feature 1')
     plt.ylabel('Feature 2')
-    plt.savefig(f'figure/dataset.png')
-
-
-    # plot loss list
-    plt.figure()
-    plt.plot(loss_list)
-    # plt.title('Loss')
-    plt.xlabel('Iteration')
-    plt.ylabel('Train loss')
-    plt.savefig(f'figure/{comment}_loss_T_{num_epochs}.png')
-
+    plt.savefig(f'figure_gaussian/{comment}_dataset.png')
 
 # plot loss list
-
+plot_dataset()
 plt.rcParams["axes.autolimit_mode"] = "round_numbers"
 plt.rcParams["axes.xmargin"] = 0
 plt.rcParams["axes.ymargin"] = 0
@@ -270,33 +250,29 @@ hessian = hessian_spectrum.Hessian(model, train_data = train_data, batch_size= 1
 
 
 
-
-# calculate full hessian
 full_hessian = hessian.get_full_hessian()
-
-
-
-print(f"frobenious norm of full hessian = {np.linalg.norm(full_hessian, 'fro' )}")
 
 # elementwise square
 full_hessian_abs = np.abs(full_hessian)
+# full_hessian = full_hessian * full_hessian #np.abs(full_hessian)
+#full_hessian = np.log(full_hessian)
 max_value = np.max(full_hessian_abs) / visual_degree
 plt.figure()
+# plt.imshow(full_hessian, cmap= 'viridis', interpolation='nearest', vmax = max_value  / visual_degree , vmin = 0)
 plt.imshow(full_hessian_abs, cmap= cmap, interpolation='nearest', vmax = max_value  , vmin = 0)
 plt.colorbar()
+#plt.title(f'Hessian of {name} step 300')
 plt.savefig(f'figure_gaussian/{comment}_fullhessian_T_{num_epochs}.png',bbox_inches='tight')
 plt.savefig(f'figure_gaussian/{comment}_fullhessian_T_{num_epochs}.pdf', bbox_inches='tight')
 plt.close()
 
 
 
-
 # average over blocks
-    
 rows_A, cols_A = full_hessian.shape
 rows_B, cols_B = rows_A// input_dim, cols_A// input_dim
 
-
+# Initialize array B
 B = np.zeros((rows_B, cols_B))
 
 # Calculate averages using nested loops
@@ -325,6 +301,7 @@ min_value = np.min(B)
 
 
 plt.figure()
+# ax = sns.heatmap(full_hessian, vmin=0, vmax=1.5 * max_value, annot=False)
 ax = sns.heatmap(B, vmin=0, vmax= max_value / 2 , annot=True, annot_kws={"size": 7}, cmap=cmap)
 
 plt.savefig(f'figure_gaussian/{comment}_avg_hessian_T_{num_epochs}.png')
@@ -333,7 +310,6 @@ plt.savefig(f'figure_gaussian/{comment}_avg_hessian_T_{num_epochs}.png')
 
 
 ## plot principal block hessian
-
 full_hessian_dic = hessian.get_full_hessian_layer_by_layer()
 
 for idx, (name, full_hessian) in enumerate(full_hessian_dic.items()):
@@ -343,8 +319,10 @@ for idx, (name, full_hessian) in enumerate(full_hessian_dic.items()):
     max_value = np.max(full_hessian)
 
     plt.figure()
+    # plt.imshow(full_hessian, cmap= 'viridis', interpolation='nearest', vmax = max_value  / visual_degree , vmin = 0)
     plt.imshow(full_hessian, cmap= cmap, interpolation='nearest', vmax = max_value  / visual_degree_block_hessian[idx] , vmin = 0)
     plt.colorbar()
+    #plt.title(f'Hessian of {name} step 300')
     plt.savefig(f'figure_gaussian/{comment}_block_hessian_{name}_T_{num_epochs}.png',bbox_inches='tight')
     plt.savefig(f'figure_gaussian/{comment}_block_hessian_{name}_T_{num_epochs}.pdf', bbox_inches='tight')
     plt.close()
